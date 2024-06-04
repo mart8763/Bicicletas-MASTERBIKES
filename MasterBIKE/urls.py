9748309e6826634bbfc553040c594442af3d5bb6
mart8763 <mart.vallejos@duocuc.cl>
2024-06-04 00:50:26 -0400

    Update con Django
    Puse todos los html donde van tambien ver los errores de los formularios y pase casi todas las carpetas hacia donde van
    Solucionar erorres

--- MasterBIKE/urls.py
+++ MasterBIKE/urls.py
@@ -3,4 +3,10 @@ from MasterBIKE import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("productos", views.productos, name="productos"),
+    path("categoria", views.categoria, name="categoria"),
+    path("carro", views.carro, name="carro"),
+    path("registro", views.registro, name="registro"),
+    path("iniciar_sesion", views.iniciar_sesion, name="iniciar_sesion"),
+    path("termino_y_condiciones", views.initermino_y_condicionesciar_sesion, name="termino_y_condiciones"),
 ]
--- MasterBIKE/views.py
+++ MasterBIKE/views.py
@@ -6,3 +6,27 @@ def index(request):
     """context: paquete variables que pueden ser consumidas por el front"""
     context = {}
     return render(request, "pages/index.html", context)
+
+def productos(request):
+    context = {}
+    return render(request, "pages/productos.html", context)
+
+def categoria(request):
+    context = {}
+    return render(request, "pages/categoria.html", context)
+
+def carro(request):
+    context = {}
+    return render(request, "pages/carro.html", context)
+
+def registro(request):
+    context = {}
+    return render(request, "pages/registro.html", context)
+
+def iniciar_sesion(request):
+    context = {}
+    return render(request, "pages/iniciar_sesion.html", context)
+
+def termino_y_condiciones(request):
+    context = {}
+    return render(request, "pages/termino_y_condiciones.html", context)
